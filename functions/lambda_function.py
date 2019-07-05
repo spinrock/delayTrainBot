be6a797ccebede8974049392a113a2d39a4527f7
Satoshi Fukai <chemistreal2@gmail.com>
2019-07-05 21:21:14 +0900

    成功時のメッセージを変更

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -34,5 +34,5 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Post Complete!')
     }
